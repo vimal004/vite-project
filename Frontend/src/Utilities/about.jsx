3e2bc78842b16da153e89fd3b1f176de4f02b1da
vimal004 <2004.vimal@gmail.com>
2024-05-26 21:35:12 +0530

    Commit 23

--- Frontend/src/Utilities/about.jsx
+++ Frontend/src/Utilities/about.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 const About = () => {
-    return (
-        <div className="">
-            <h1>About Us</h1>
-        </div>
-    );
-}
+  return (
+    <div className="">
+      <h1>About Us</h1>
+    </div>
+  );
+};
 
 export default About;
